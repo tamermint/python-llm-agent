b190fbbd93b0492e1a9fca1193cb5a49a91b2caf
Vivek <vivek.mitra@outlook.com>
2026-07-05 17:29:47 +1000

    Feature: get and list files within working directory

---
+++ functions/get_files_info.py
@@ -0,0 +1,31 @@
+import os
+
+def get_files_info(working_directory: str, directory: str = ".") -> str:
+    try:
+        abs_path_work_dir = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(abs_path_work_dir, directory))
+
+        valid_target_dir = os.path.commonpath([abs_path_work_dir, target_dir]) == abs_path_work_dir
+        
+        if(directory == "."):
+            print(f'Result for current directory:')
+        else:
+            print(f'Result for {directory} directory:')
+        if not valid_target_dir:
+            print(f'Cannot list "{directory}" as it is outside the permitted working directory')
+        elif not os.path.isdir(target_dir):
+            print(f'"{directory}" is not a directory')
+        else:
+            for file in os.listdir(target_dir):
+                file_path = os.path.join(target_dir, file)
+                file_size = os.path.getsize(file_path)
+                is_dir = os.path.isdir(file_path)
+                print(f'- {file}: file_size={file_size}, is_dir={is_dir}')
+    except Exception as e:
+        print(f'Error: {e}')
+
+def main():
+    get_files_info("calculator", ".")
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,6 +1,47 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+import argparse
+
+
+def chatWithAI():
+    try:
+        load_dotenv()
+        api_key = os.environ.get("OPENROUTER_API_KEY")
+        client = OpenAI(    
+            base_url="https://openrouter.ai/api/v1",
+            api_key=api_key
+        )
+
+        parser = argparse.ArgumentParser(description="LLM Chatbot")
+        parser.add_argument("user_prompt", type=str, help="User prompt")
+        parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+        args = parser.parse_args()
+
+        messages = [
+            {
+                "role": "user",
+                "content": args.user_prompt,
+            }
+        ]
+
+        response = client.chat.completions.create(model="openrouter/free", messages=messages)
+        if response.usage is None:
+            raise RuntimeError("No usage indicated! Check API parametres")
+        prompt_tokens = response.usage.prompt_tokens
+        response_tokens = response.usage.completion_tokens
+        if(args.verbose):
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {prompt_tokens}")
+            print(f"Response tokens: {response_tokens}")
+            print(response.choices[0].message.content)
+        else:
+            print(response.choices[0].message.content)
+    except Exception as e:
+        print(f"Unknown error occured: {e}")
+
+
 def main():
-    print("Hello from python-llm-agent!")
-
-
+    chatWithAI()
 if __name__ == "__main__":
     main()
